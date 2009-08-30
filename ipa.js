7781c0f76a358fc1be80ea844c954e86c4a94cf8
Anika Henke <anika@selfthinker.org>
2009-08-30 19:40:28 +0100

    version 2009-08-30
    * fixed URL to popup (which sometimes caused trouble in subfolders)
    * use icon from same folder (no need to move it anymore)
    * deleted action.php as it was only there for the plugin manager, but the plugin also works without it
    * added comments to all files
    * fixed lang of ipa.html
    * added README

--- ipa.js
+++ ipa.js
@@ -1,17 +1,27 @@
+/**
+ * JavaScript for the Typewriter Popup for the IPA Plugin
+ *
+ * @author     Anika Henke <anika@selfthinker.org>
+ * @author     http://de.wikipedia.org/wiki/Wikipedia:Helferlein/IPA_Typewriter
+ */
 
 window.onload = function(){
+    // if the popup was opened from the toolbar, don't display the 'copy to clipboard' tools
     if(opener) document.getElementById("copyIpa").style.display = 'none';
 }
 
 function InsertChar(sUnicodeCode) {
     if(!opener){
+        // if the popup was NOT opened from the toolbar, insert each selected char into the 'copy to clipboard' text box
         document.getElementById("txtIpa").focus();
         document.getElementById("txtIpa").value += String.fromCharCode(parseInt(sUnicodeCode, 16));
     } else {
+        // if the popup was opened from the toolbar, insert each selected char into the wiki text
         opener.insertTags('wiki__text',String.fromCharCode(parseInt(sUnicodeCode, 16)),'','');
     }
 }
 
+// copy text into clipboard (if possible, else select the text)
 function CopyIpaToClipboard() {
     if (window.clipboardData) {
         window.clipboardData.setData('Text', document.getElementById("txtIpa").value);
@@ -20,4 +30,3 @@ function CopyIpaToClipboard() {
         document.getElementById("txtIpa").select();
     }
 }
-
--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-if(toolbar){
+/**
+ * JavaScript for the IPA Plugin
+ * - adds the button for the typewriter popup to the toolbar
+ *
+ * @author     Anika Henke <anika@selfthinker.org>
+ */
+
+if(window.toolbar!=undefined) {
   toolbar[toolbar.length] = {
     "type":"mediapopup",
     "title":"IPA",
-    "icon":"ipa.png",
-    "url":DOKU_BASE+"lib/plugins/ipa/ipa.html?ns=",
+    "icon":"../../plugins/ipa/ipa.png",
+    "url":"lib/plugins/ipa/ipa.html?id=",
     "name":"ipa_chars",
     "options":"width=680,height=470,left=20,top=20,scrollbars=yes,resizable=yes"
   };
